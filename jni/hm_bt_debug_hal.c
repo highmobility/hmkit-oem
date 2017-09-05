8c7368878a4d2c592ba0934142c0359ad698151d
Maidu Ule <maidu@high-mobility.com>
2017-09-05 15:24:11 +0300

    Turned on logging

--- jni/hm_bt_debug_hal.c
+++ jni/hm_bt_debug_hal.c
@@ -1,14 +1,22 @@
 
 #include "hm_bt_debug_hal.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 void hm_bt_debug_hal_log(const char *str, ...){
   va_list args;
-  va_start(args, str);
-  //debug_log_args(str, args);
-  va_end(args);
+    va_start(args, str);
+    char output[3000];
+    vsprintf(output, str, args);
+    printf(output,"Usage %s, [options] ... ");
+    printf("\n");
+    va_end(args);
 }
 
 void hm_bt_debug_hal_log_hex(const uint8_t *data, const uint16_t length){
-  //debug_log_hex(data, length);
+  uint16_t i;
+    for(i = 0 ; i < length ; i++){
+      printf("%02X ", data[i]);
+    }
+    printf("\n");
 }
--- src/com/highmobility/Main.java
+++ src/com/highmobility/Main.java
@@ -5,7 +5,6 @@ import com.highmobility.btcore.*;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("HELLO");
 
 	CoreCallback callback = new CoreCallback();
 
