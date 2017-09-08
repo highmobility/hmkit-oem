917a60d5bd578a7e0cf46dadef68f58eda8df79b
Tõnis Tiganik <ttiganik@gmail.com>
2017-09-08 13:04:21 +0300

    add auto api jar

--- src/main/java/com/highmobility/Main.java
+++ src/main/java/com/highmobility/Main.java
@@ -1,16 +1,15 @@
 package com.highmobility;
 
-import com.highmobility.btcore.*;
+import com.highmobility.btcore.HMBTCore;
+
 
 public class Main {
 
     public static void main(String[] args) {
+		CoreCallback callback = new CoreCallback();
+		HMBTCore core = new HMBTCore();
+		core.HMBTCoreInit(callback);
 
-	CoreCallback callback = new CoreCallback();
 
-	HMBTCore core = new HMBTCore();
-
-	core.HMBTCoreInit(callback);
-
-    }
+	}
 }
