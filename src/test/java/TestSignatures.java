a9d0c2fc12ec0807b89224bc956033626d36d872
ttiganik <ttiganik@gmail.com>
2019-06-21 07:28:05 +0300

    update make file

--- src/test/java/TestSignatures.java
+++ src/test/java/TestSignatures.java
@@ -17,7 +17,6 @@ public class TestSignatures {
                 ("A5A74048A85AC52A2E41DE5F9554C9CC36B6E3721EE8E8CE9169DC54192D17FD52C3BD1A4AE7F592756C083E17E54B7730965D99B238EB8D33B172DC35E32398");
         PrivateKey privateKey = new PrivateKey
                 ("1B8593D0478B9017C2427256AAEE25FF8A4E20EC6611AFE31D52B32CE0BECCA2");
-
         assertTrue(true);
 
         Bytes data = new Bytes("0203");
@@ -27,6 +26,7 @@ public class TestSignatures {
 
         publicKey = new PublicKey("A6A74048A85AC52A2E41DE5F9554C9CC36B6E3721EE8E8CE9169DC54192D17FD52C3BD1A4AE7F592756C083E17E54B7730965D99B238EB8D33B172DC35E32398");
         assertTrue(crypto.verify(data, sig, publicKey) == false);
+
 
         // TODO: 2019-06-17 test the telematics command as well
     }
